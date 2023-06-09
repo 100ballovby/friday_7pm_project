12d40f90aef42f7e30735e86c2e3481dd71c2dac
greatraksin <greatraksin@icloud.com>
2023-06-09 18:23:49 +0300

    Adding bars

---
+++ script.js
@@ -0,0 +1,12 @@
+function dark_mode() {
+    let body = document.querySelector('body');
+
+    if (body.classList.contains('dark')) {
+        body.classList.remove('dark');
+    } else {
+        body.classList.add('dark');
+    }
+}
+
+
+
