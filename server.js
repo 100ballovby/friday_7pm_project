ed20213c6ed3c5bf7678a931a24f77579847bff3
greatraksin <greatraksin@icloud.com>
2023-06-30 17:58:45 +0300

    Adding form listener

--- server.js
+++ server.js
@@ -1,7 +1,26 @@
 const express = require('express');
 const path = require('path');
+const fs = require("fs");
 
 const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.post('/submit-form', (req, res) => {
+    const { name, email, subject, message } = req.body;
+    const data = `Name: ${name}\nEmail: ${email}\nSubject ${subject}\nSubject: ${message}\n\n`;
+
+    fs.appendFile(path.resolve(__dirname, 'data.txt'), data, (err) => {
+        if (err) {
+            console.error(err);
+            res.sendStatus(500);
+        } else {
+            console.log('Data saved successfully.');
+            res.sendStatus(200);
+        }
+    });
+});
 
 app.use('/static', express.static(path.resolve(__dirname, "frontend", "static")));
 app.get("/", (req, res) => {
